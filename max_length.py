2bbf9497b79027a5cc39dad8e922a7b6167e98bf
Diego Carranza Ortiz <59874504+diegoaco@users.noreply.github.com>
2021-06-08 22:07:32 -0500

    Update max_length.py

--- max_length.py
+++ max_length.py
@@ -6,8 +6,6 @@ import random
 n = 1000
 c, m = 0, 0
 for i in range(1,n+1):
-    x = [random.uniform(0,1)]
-    a = x[0]
-    b = 1 - x[0]
-    m = m + max(a,b)
+    x = random.uniform(0,1)
+    m = m + max(x, 1-x)
 print('Expected length:', m/n)
