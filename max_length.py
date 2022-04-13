5571954349f12f8ae78494b0d6886840ab66a12b
Diego Carranza Ortiz <59874504+diegoaco@users.noreply.github.com>
2022-04-13 17:08:24 -0500

    Update max_length.py

--- max_length.py
+++ max_length.py
@@ -1,11 +1,22 @@
-''' Simulates n expriments where we cut a unit stick
+'''Simulates n expriments where we cut a unit stick
 and finds the expected value of the largest piece '''
 
-import random
+import random as rd
 
-n = 1000
+# Cut into two parts:
+n = 5000
 m = 0
-for i in range(1,n+1):
-    x = random.uniform(0,1)
+for i in range(1, n+1):
+    x = rd.uniform(0,1)
     m = m + max(x, 1-x)
+print('Expected length: ', m/n)
+
+# Cut into three parts:
+n = 5000
+m = 0
+for i in range(1, n+1):
+    x = rd.uniform(0,1)
+    y = rd.uniform(0,1)
+    l = [min(x,y), abs(x-y), 1 - max(x,y)]
+    m = m + max(min(x,y), abs(x-y), 1 - max(x,y))
 print('Expected length:', m/n)
